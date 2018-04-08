22c2766ad11e5621a8ad345ef8b042e2a3740abc
Chris Stryczynski <chris@soundshare.co.uk>
2018-04-08 13:32:46 +0000

    woohoo it works

--- src/Main.hs
+++ src/Main.hs
@@ -1,4 +1,68 @@
+{-# LANGUAGE LambdaCase #-}
 module Main where
 
+
+import System.IO (stdin, hSetEcho, hReady, stdout)
+import GHC.IO.Handle
+import Control.Monad (when)
+import System.Process
+import Control.Monad.STM
+import Control.Concurrent.STM.TMVar
+import Control.Concurrent
+import Control.Monad
+
+
+getKey :: IO [Char]
+getKey = reverse <$> getKey' ""
+  where getKey' chars = do
+          char <- getChar
+          more <- hReady stdin
+          (if more then getKey' else return) (char:chars)
+
+type Commands = TMVar [String]
+
+ghci :: Commands -> IO ()
+ghci cmds = createProcess ((proc "ghci" ["-ignore-dot-ghci"])) {std_in=CreatePipe} >>= \case
+    (Just pin, Nothing, Nothing, ph) -> processCommandsLoop cmds pin
+    _ -> error "Something went wrong"
+
+processCommandsLoop :: Commands -> Handle ->  IO ()
+processCommandsLoop cmds input = do
+  x <- atomically $ takeTMVar cmds
+  forM x (\x' -> do
+    print "-- ## Sending command to process:"
+    print x'
+    hPutStr input (x' ++ "\n")
+    hFlush input
+    )
+  hFlush stdout
+  processCommandsLoop cmds input
+
+mainLoop :: TMVar [String] -> IO ()
+mainLoop commands = do
+  key <- getKey
+  when (key /= "\ESC") $ do
+    case key of
+      -- "s" -> do
+      --   putStrLn "-- ## Starting!"
+      --   ghci
+      "r" -> putStrLn "-- ## Reloading!"
+      "t" -> do
+        putStrLn "-- ## Testing!"
+        atomically $ putTMVar commands ([":t head", ":t tail"])
+      "h" -> putStrLn "-- ## Hushing!"
+      "d" -> do
+        putStrLn "-- ## Debug!"
+        (atomically $ readTMVar commands) >>= print
+      _        -> return ()
+  mainLoop commands
+
 main :: IO ()
-main = putStrLn "Hello, Haskell!"
+main = do
+  hSetBuffering stdout NoBuffering
+  hSetBuffering stdin NoBuffering
+  cmds <- newEmptyTMVarIO
+  ghciThread <- forkIO $ ghci cmds
+  print "Started"
+  hSetEcho stdin False
+  mainLoop cmds
